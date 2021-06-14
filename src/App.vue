59089534ac077525c90b5d3feb4d09424cf40c87
darkbright <foxtori8946@gmail.com>
2021-06-14 11:08:54 +0900

    Header - Nav

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
+  <Header />
   <RouterView />
 </template>
+
+<script>
+import Header from '~/components/Header'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
 
 <style land="scss">
 @import "~/scss/main"
