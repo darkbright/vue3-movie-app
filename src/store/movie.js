16a6c8c235bc2c73ea4d7d2138f756d86a37a224
darkbright <foxtori8946@gmail.com>
2021-06-15 09:18:33 +0900

    Search Movie - 추가 요청, 중복 제거

--- src/store/movie.js
+++ src/store/movie.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import _uniqBy from 'lodash/uniqBy'
 
 export default {
   namespaced: true,
@@ -20,15 +21,35 @@ export default {
     }
   },
   actions: {
-    async searchMovies({ commit }, payload) {
+    async searchMovies({ state, commit }, payload) {
       const { title, type, number, year } = payload
       const OMDB_API_KEY = '4be3b0be'
 
       const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}page=1`)
       const { Search, totalResults } = res.data
       commit('updateState', {
-        movies: Search
+        movies: _uniqBy(Search, 'imdbID')
       })
+      console.log(totalResults) // 268 => 27
+      console.log(typeof totalResults) // string
+
+      const total = parseInt(totalResults, 10)
+      const pageLength = Math.ceil(total / 10)
+
+      // 추가 요청!
+      if (pageLength > 1) {
+        for (let page = 2; page <= pageLength; page += 1) {
+          if (page > number / 10) break
+          const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}page=${page}`)
+          const { Search } = res.data
+          commit('updateState', {
+            movies: [
+              ...state.movies, 
+              ..._uniqBy(Search, 'imdbID')
+            ]
+          })
+        }
+      }
     }
   }
 }
