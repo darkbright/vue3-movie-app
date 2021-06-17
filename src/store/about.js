544cc83e9fec6781ebd8d8dc83051e720acdc15f
darkbright <foxtori8946@gmail.com>
2021-06-17 14:27:52 +0900

    About menu

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () =>({
+    name: 'DARKBRIGHT',
+    email: 'darkbright@kakao.com',
+    blog: 'https://www.github.com',
+    phone: '+82-10-1234-5678',
+    image: 'https://heropy.blog/css/images/logo.png'
+  })
+}
